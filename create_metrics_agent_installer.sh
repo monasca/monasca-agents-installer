869815cd8941142c77dee2f8a36e29c4d524788e
Jakub Wachowski <jakub.wachowski@ts.fujitsu.com>
2017-08-25 12:46:29 +0200

    Run monasca-setup after installing the agent
    Change-Id: I9633d8714b1a50db3c88b6de676dd9eb0d089212

--- create_metrics_agent_installer.sh
+++ create_metrics_agent_installer.sh
@@ -3,20 +3,30 @@ source commons
 
 MONASCA_AGENT_TMP_DIR="${TMP_DIR}/monasca-agent"
 
-rm -rf ${MONASCA_AGENT_TMP_DIR}
 mkdir -p ${MONASCA_AGENT_TMP_DIR}
 
 virtualenv ${MONASCA_AGENT_TMP_DIR}
 
 ${MONASCA_AGENT_TMP_DIR}/bin/pip install monasca-agent
+cp configure_metrics_agent.sh ${MONASCA_AGENT_TMP_DIR}/bin
 
 if [ -d "${MAKESELF_DIR}" ]; then
-  cd ${MAKESELF_DIR}
-  git pull
-  cd -
+    cd ${MAKESELF_DIR}
+    git pull
+    cd -
 else
-  git clone ${MAKESELF_REPO} ${MAKESELF_DIR}
+    git clone ${MAKESELF_REPO} ${MAKESELF_DIR}
 fi
 
-${MAKESELF_DIR}/makeself.sh --notemp ${MONASCA_AGENT_TMP_DIR} monasca-agent.run "Monasca Agents installer" echo "Here goes the configuration step..."
+cat metrics_agent_help_header > metrics_agent_help_header.tmp
+${MONASCA_AGENT_TMP_DIR}/bin/python ${MONASCA_AGENT_TMP_DIR}/bin/monasca-setup --help \
+    >> metrics_agent_help_header.tmp
 
+${MAKESELF_DIR}/makeself.sh --notemp \
+                            --help-header metrics_agent_help_header.tmp \
+                            ${MONASCA_AGENT_TMP_DIR} \
+                            monasca-agent.run \
+                            "Monasca Agents installer" \
+                            ./bin/configure_metrics_agent.sh
+
+rm -rf ${MONASCA_AGENT_TMP_DIR}
