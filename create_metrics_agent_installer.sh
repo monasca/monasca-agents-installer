a1b02d1899271599ca81ceea9d80eb92edf3a715
Denis Poisson <denis.poisson@ts.fujitsu.com>
2017-09-15 13:03:35 +0200

    Add the ability to specify version of monasca agent
    Change-Id: If7dc715c29c44e5ac39c63b703b0a77e667184a6

--- create_metrics_agent_installer.sh
+++ create_metrics_agent_installer.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 source commons
+
+#takes the first argument as the version. Defaults to the latest version of monasca-agent if no argument is specified.
+MONASCA_AGENT_VERSION=${1:-`pip search monasca-agent | grep monasca-agent | awk '{print $2}' | sed 's|(||' | sed 's|)||'`}
 
 MONASCA_AGENT_TMP_DIR="${TMP_DIR}/monasca-agent"
 
@@ -7,7 +10,7 @@ mkdir -p ${MONASCA_AGENT_TMP_DIR}
 
 virtualenv ${MONASCA_AGENT_TMP_DIR}
 
-${MONASCA_AGENT_TMP_DIR}/bin/pip install monasca-agent
+${MONASCA_AGENT_TMP_DIR}/bin/pip install monasca-agent==$MONASCA_AGENT_VERSION
 cp configure_metrics_agent.sh ${MONASCA_AGENT_TMP_DIR}/bin
 
 if [ -d "${MAKESELF_DIR}" ]; then
@@ -22,10 +25,11 @@ cat metrics_agent_help_header > metrics_agent_help_header.tmp
 ${MONASCA_AGENT_TMP_DIR}/bin/python ${MONASCA_AGENT_TMP_DIR}/bin/monasca-setup --help \
     >> metrics_agent_help_header.tmp
 
+
 ${MAKESELF_DIR}/makeself.sh --notemp \
                             --help-header metrics_agent_help_header.tmp \
                             ${MONASCA_AGENT_TMP_DIR} \
-                            monasca-agent.run \
+                            monasca-agent-${MONASCA_AGENT_VERSION}.run \
                             "Monasca Agents installer" \
                             ./bin/configure_metrics_agent.sh
 
