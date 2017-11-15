86cf2dc8e6b459711f7d73ed837edb10025ae350
Haruki Yamanashi <h.yamanashi@jp.fujitsu.com>
2017-11-15 17:38:08 +0900

    Create monasca ui package under upper constraints
    This change makes it possible to create monasca ui package with upper
    constraints file and prevent package compatibility issue.

--- create_monasca_ui_installer.sh
+++ create_monasca_ui_installer.sh
@@ -5,13 +5,19 @@ source commons
 # of monasca-ui if no argument is specified.
 MONASCA_UI_VERSION=${1:-`pip search monasca-ui | grep monasca-ui | \
                             awk '{print $2}' | sed 's|(||' | sed 's|)||'`}
+UPPER_CONSTRAINT_FILE=${2:-""}
 
 MONASCA_UI_TMP_DIR="${TMP_DIR}/monasca-ui"
 
 mkdir -p ${MONASCA_UI_TMP_DIR}
 
 virtualenv ${MONASCA_UI_TMP_DIR}
-${MONASCA_UI_TMP_DIR}/bin/pip install monasca-ui==$MONASCA_UI_VERSION
+
+if [ -z "${UPPER_CONSTRAINT_FILE}" ]; then
+    ${MONASCA_UI_TMP_DIR}/bin/pip install monasca-ui==$MONASCA_UI_VERSION
+else
+    ${MONASCA_UI_TMP_DIR}/bin/pip install -c ${UPPER_CONSTRAINT_FILE} monasca-ui==$MONASCA_UI_VERSION
+fi
 virtualenv --relocatable ${MONASCA_UI_TMP_DIR}
 
 cp configure_monasca_ui.sh ${MONASCA_UI_TMP_DIR}/bin
