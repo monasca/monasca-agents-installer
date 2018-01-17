490767b66709b4a2faef5b4de7ac233fa6d6898a
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2018-01-17 12:48:41 +0100

    Fix shellcheck errors in create_monasca_ui_installer.sh
    Change-Id: Ie98faa9cbd026c2783459eb7bcefa668b8b0102f

--- create_monasca_ui_installer.sh
+++ create_monasca_ui_installer.sh
@@ -1,44 +1,59 @@
 #!/bin/bash -e
+
+# shellcheck disable=SC1091
 source commons
 
 # takes the first argument as the version. Defaults to the latest version
 # of monasca-ui if no argument is specified.
-MONASCA_UI_VERSION=${1:-`pip search monasca-ui | grep monasca-ui | \
-                            awk '{print $2}' | sed 's|(||' | sed 's|)||'`}
+MONASCA_UI_VERSION=${1:-$(pip search monasca-ui | grep monasca-ui | \
+                            awk '{print $2}' | sed 's|(||' | sed 's|)||')}
 UPPER_CONSTRAINT_FILE=${2:-""}
 
 MONASCA_UI_TMP_DIR="${TMP_DIR}/monasca-ui"
 
-mkdir -p ${MONASCA_UI_TMP_DIR}
+mkdir -p "${MONASCA_UI_TMP_DIR}"
 
-virtualenv ${MONASCA_UI_TMP_DIR}
+echo ">>> Creating virtual environment in temporary location"
+virtualenv "${MONASCA_UI_TMP_DIR}"
 
+echo ">>> Downloading monasca-ui in version: ${MONASCA_UI_VERSION}"
 if [ -z "${UPPER_CONSTRAINT_FILE}" ]; then
-    ${MONASCA_UI_TMP_DIR}/bin/pip install monasca-ui==$MONASCA_UI_VERSION
+    echo ">>> No upper constraint file specified"
+    "${MONASCA_UI_TMP_DIR}"/bin/pip install monasca-ui=="$MONASCA_UI_VERSION"
 else
-    ${MONASCA_UI_TMP_DIR}/bin/pip install -c ${UPPER_CONSTRAINT_FILE} monasca-ui==$MONASCA_UI_VERSION
+    echo ">>> Using upper constraint file: ${UPPER_CONSTRAINT_FILE}"
+    "${MONASCA_UI_TMP_DIR}"/bin/pip install -c "${UPPER_CONSTRAINT_FILE}" \
+      monasca-ui=="$MONASCA_UI_VERSION"
 fi
-virtualenv --relocatable ${MONASCA_UI_TMP_DIR}
 
-cp configure_monasca_ui.sh ${MONASCA_UI_TMP_DIR}/bin
 LOCAL_SETTINGS_FILE=$(find "${MONASCA_UI_TMP_DIR}" -name local_settings.py)
 sed -i -e "s/192.168.10.4:5601/<kibana_host>:5601/g" "${LOCAL_SETTINGS_FILE}"
 
+virtualenv --relocatable "${MONASCA_UI_TMP_DIR}"
+
+cp configure_monasca_ui.sh "${MONASCA_UI_TMP_DIR}"/bin
+
+echo ">>> Downloading latest Makeself"
 if [ -d "${MAKESELF_DIR}" ]; then
-    cd ${MAKESELF_DIR}
+    cd "${MAKESELF_DIR}" || exit
     git pull
     cd -
 else
-    git clone ${MAKESELF_REPO} ${MAKESELF_DIR}
+    git clone "${MAKESELF_REPO}" "${MAKESELF_DIR}"
 fi
 
 cat monasca_ui_help_header > monasca_ui_help_header.tmp
 
-${MAKESELF_DIR}/makeself.sh --notemp \
-                            --help-header monasca_ui_help_header.tmp \
-                            ${MONASCA_UI_TMP_DIR} \
-                            monasca-ui-${MONASCA_UI_VERSION}.run \
-                            "Monasca UI installer" \
-                            ./bin/configure_monasca_ui.sh
+echo ">>> Creating monasca-ui installer file"
+"${MAKESELF_DIR}"/makeself.sh --notemp \
+                              --tar-quietly \
+                              --help-header monasca_ui_help_header.tmp \
+                              "${MONASCA_UI_TMP_DIR}" \
+                              monasca-ui-"${MONASCA_UI_VERSION}".run \
+                              "Monasca UI installer" \
+                              ./bin/configure_monasca_ui.sh
 
-rm -rf ${MONASCA_UI_TMP_DIR}
+echo ">>> Removing temporary files"
+rm -rf "${MONASCA_UI_TMP_DIR}"
+
+echo ">>> Process of creating monasca-ui installer ended successfully"
