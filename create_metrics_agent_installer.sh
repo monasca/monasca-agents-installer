9a1858612e9cbe396ff9fa4d8eb086cbac2e20d8
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2018-01-29 11:22:54 +0100

    Fix Shellcheck warns in matrics agent installer
    Add messages to creation of the Monasca log api output plugin process.
    Tar quietly.
    
    Change-Id: I1bee89e2f4d5dc6e7a47671b199e8ef01728670f
    Signed-off-by: Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>

--- create_metrics_agent_installer.sh
+++ create_metrics_agent_installer.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
+
+# shellcheck disable=SC1091
 source commons
 
 # takes the first argument as the version. Defaults to the latest version
 # of monasca-agent if no argument is specified.
-MONASCA_AGENT_VERSION=`pip search monasca-agent | grep monasca-agent | \
-                        awk '{print $2}' | sed 's|(||' | sed 's|)||'`
+MONASCA_AGENT_VERSION=$(pip search monasca-agent | grep monasca-agent | \
+                        awk '{print $2}' | sed 's|(||' | sed 's|)||')
 UPPER_CONSTRAINTS_FILE=""
 INSTALL_LIBVIRT_DEPENDENCIES=false
 
@@ -36,52 +38,64 @@ MONASCA_AGENT_TMP_DIR="${TMP_DIR}/monasca-agent"
 PIP_DEPENDENCIES=("pymysql")
 LIBVIRT_DEPENDENCIES=("libvirt-python" "lxml" "python-neutronclient" "python-novaclient")
 
-mkdir -p ${MONASCA_AGENT_TMP_DIR}
+mkdir -p "${MONASCA_AGENT_TMP_DIR}"
 
-virtualenv ${MONASCA_AGENT_TMP_DIR}
+echo ">>> Creating virtual environment in temporary location"
+virtualenv "${MONASCA_AGENT_TMP_DIR}"
 
+echo ">>> Downloading Monasca Agent in version: ${MONASCA_AGENT_VERSION}"
 if [ -z "${UPPER_CONSTRAINTS_FILE}" ]; then
-    ${MONASCA_AGENT_TMP_DIR}/bin/pip install monasca-agent==$MONASCA_AGENT_VERSION
-    for pip_dependencies in ${PIP_DEPENDENCIES[@]}; do
-        ${MONASCA_AGENT_TMP_DIR}/bin/pip install $pip_dependencies
+    echo ">>> No upper constraints file specified"
+    "${MONASCA_AGENT_TMP_DIR}"/bin/pip install monasca-agent=="$MONASCA_AGENT_VERSION"
+    for pip_dependencies in "${PIP_DEPENDENCIES[@]}"; do
+        "${MONASCA_AGENT_TMP_DIR}"/bin/pip install "$pip_dependencies"
     done
     if [ ${INSTALL_LIBVIRT_DEPENDENCIES} = true ]; then
-        for libvirt_dependencies in ${LIBVIRT_DEPENDENCIES[@]}; do
-            ${MONASCA_AGENT_TMP_DIR}/bin/pip install $libvirt_dependencies
+        echo ">>> Downloading libvirt dependencies"
+        for libvirt_dependencies in "${LIBVIRT_DEPENDENCIES[@]}"; do
+            "${MONASCA_AGENT_TMP_DIR}"/bin/pip install "$libvirt_dependencies"
         done
     fi
 else
-    ${MONASCA_AGENT_TMP_DIR}/bin/pip install -c ${UPPER_CONSTRAINTS_FILE} monasca-agent==$MONASCA_AGENT_VERSION
-    for dependencies in ${PIP_DEPENDENCIES[@]}; do
-        ${MONASCA_AGENT_TMP_DIR}/bin/pip install -c ${UPPER_CONSTRAINTS_FILE} $dependencies
+    echo ">>> Using upper constraints file: ${UPPER_CONSTRAINTS_FILE}"
+    "${MONASCA_AGENT_TMP_DIR}"/bin/pip install -c "${UPPER_CONSTRAINTS_FILE}" monasca-agent=="$MONASCA_AGENT_VERSION"
+    for dependencies in "${PIP_DEPENDENCIES[@]}"; do
+        "${MONASCA_AGENT_TMP_DIR}"/bin/pip install -c "${UPPER_CONSTRAINTS_FILE}" "$dependencies"
     done
     if [ ${INSTALL_LIBVIRT_DEPENDENCIES} = true ]; then
-        for libvirt_dependencies in ${LIBVIRT_DEPENDENCIES[@]}; do
-            ${MONASCA_AGENT_TMP_DIR}/bin/pip install -c ${UPPER_CONSTRAINTS_FILE} $libvirt_dependencies
+        echo ">>> Downloading libvirt dependencies"
+        for libvirt_dependencies in "${LIBVIRT_DEPENDENCIES[@]}"; do
+            "${MONASCA_AGENT_TMP_DIR}"/bin/pip install -c "${UPPER_CONSTRAINTS_FILE}" "$libvirt_dependencies"
         done
     fi
 fi
-virtualenv --relocatable ${MONASCA_AGENT_TMP_DIR}
+virtualenv --relocatable "${MONASCA_AGENT_TMP_DIR}"
 
-cp configure_metrics_agent.sh ${MONASCA_AGENT_TMP_DIR}/bin
+cp configure_metrics_agent.sh "${MONASCA_AGENT_TMP_DIR}"/bin
 
+echo ">>> Downloading latest Makeself"
 if [ -d "${MAKESELF_DIR}" ]; then
-    cd ${MAKESELF_DIR}
+    cd "${MAKESELF_DIR}" || exit
     git pull
     cd -
 else
-    git clone ${MAKESELF_REPO} ${MAKESELF_DIR}
+    git clone "${MAKESELF_REPO}" "${MAKESELF_DIR}"
 fi
 
 cat metrics_agent_help_header > metrics_agent_help_header.tmp
-${MONASCA_AGENT_TMP_DIR}/bin/python ${MONASCA_AGENT_TMP_DIR}/bin/monasca-setup --help \
+"${MONASCA_AGENT_TMP_DIR}"/bin/python "${MONASCA_AGENT_TMP_DIR}"/bin/monasca-setup --help \
     >> metrics_agent_help_header.tmp
 
-${MAKESELF_DIR}/makeself.sh --notemp \
-                            --help-header metrics_agent_help_header.tmp \
-                            ${MONASCA_AGENT_TMP_DIR} \
-                            monasca-agent-${MONASCA_AGENT_VERSION}.run \
-                            "Monasca Agents installer" \
-                            ./bin/configure_metrics_agent.sh
+echo ">>> Creating Monasca Agent installer file"
+"${MAKESELF_DIR}"/makeself.sh --notemp \
+                              --tar-quietly \
+                              --help-header metrics_agent_help_header.tmp \
+                              "${MONASCA_AGENT_TMP_DIR}" \
+                              monasca-agent-"${MONASCA_AGENT_VERSION}".run \
+                              "Monasca Agent installer" \
+                              ./bin/configure_metrics_agent.sh
 
-rm -rf ${MONASCA_AGENT_TMP_DIR}
+echo ">>> Removing temporary files"
+rm -rf "${MONASCA_AGENT_TMP_DIR}"
+
+echo ">>> Process of creating Monasca Agent installer ended successfully"
